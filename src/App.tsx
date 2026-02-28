6d22cb29f17f81d0c71dee995c49cdbde5f46644
SpacePanda7077 <spacepanda7077@gmail.com>
2026-02-28 11:23:39 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import { useCallback, useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ConnectButton from "./Components/ConnectionBtn";
+import {
+  useWriteContract,
+  useReadContract,
+  useWriteContractSync,
+  useTransactionConfirmations,
+} from "wagmi";
+import { useAppKitAccount } from "@reown/appkit/react";
+
+import { CA, abi, token } from "./Blockchain/blockchain";
+import { erc20Abi, maxUint256, parseAbi, parseEther } from "viem";
+
+function App() {
+  const [depositAmount, setDepositAmount] = useState(0);
+  const [withdrawAmount, setWithdrawAmount] = useState(0);
+  const [allowance, setAllowance] = useState(0n);
+  const [allowanceEnough, setAllowanceEnough] = useState(true);
+  const {
+    data: depositData,
+    error: depositError,
+    mutate,
+  } = useWriteContractSync();
+
+  const {
+    data: getAllowanceData,
+    error: getAllowanceError,
+    mutate: getAllowanceMutate,
+  } = useWriteContractSync();
+  const { address, isConnected } = useAppKitAccount();
+  const { data: allowanceData, error: allowanceError } = useReadContract({
+    address: token,
+    abi: erc20Abi,
+    functionName: "allowance",
+    args: [address as `0x${string}`, CA],
+  });
+
+  const getAllowance = () => {
+    getAllowanceMutate({
+      address: token,
+      abi: erc20Abi,
+      functionName: "approve",
+      args: [CA, maxUint256],
+    });
+  };
+  const handleDeposits = useCallback(async () => {
+    if (depositAmount <= 0) {
+      alert("increase deposit amount");
+      return;
+    }
+    // getAllowance();
+
+    mutate({
+      address: CA,
+      abi: abi.abi,
+      functionName: "deposit",
+      args: [parseEther(depositAmount.toString())],
+    });
+  }, [address, depositAmount]);
+
+  useEffect(() => {
+    if (allowanceData) {
+      console.log(allowanceData);
+      setAllowance(allowanceData);
+    }
+    console.log(allowanceData);
+  }, [allowanceData]);
+
+  useEffect(() => {
+    if (getAllowanceError) {
+      console.log(getAllowanceError);
+    }
+  }, [getAllowanceError]);
+
+  return (
+    <>
+      <ConnectButton />
+      <div>
+        <h1>Test Backend</h1>
+        <div className="deposit_cont">
+          <input
+            type="number"
+            onChange={(e) => {
+              setDepositAmount(Number(e.target.value));
+            }}
+          />
+          <button onClick={handleDeposits}>Deposit</button>
+        </div>
+        <div className="withdraw_cont">
+          <input
+            type="number"
+            onChange={(e) => {
+              setWithdrawAmount(Number(e.target.value));
+            }}
+          />
+          <button>Withdraw</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,41 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createAppKit } from "@reown/appkit/react";
+
+import { WagmiProvider } from "wagmi";
+import { somniaTestnet } from "@reown/appkit/networks";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import "./index.css";
+import App from "./App.tsx";
+
+const queryClient = new QueryClient();
+
+// 1. Get projectId from https://dashboard.reown.com
+const projectId = "YOUR_PROJECT_ID";
+
+const networks = [somniaTestnet];
+
+// 4. Create Wagmi Adapter
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: true,
+});
+
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks: [somniaTestnet],
+  projectId,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>{<App />}</QueryClientProvider>
+    </WagmiProvider>
+  </StrictMode>,
+);
